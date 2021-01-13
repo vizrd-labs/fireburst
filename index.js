55d5c7f7553416cfe85f5b93a1aef16588bb81f5
vizrdlabs <64224945+vizrdlabs@users.noreply.github.com>
2021-01-13 05:56:25 +0530

    Added Comments

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-// const cors = require("cors");
+// Made by Raunak Sudhakar, as ViZrdLabs.
+
+// You need two devices to run it.
+// Demo
+// "Controller" on: https://fireburst.herokuapp.com/
+// "Screen" on: https://fireburst.herokuapp.com/screen/
+// Allows multiple devices as "controllers"
+// to send "Fireworks" to the "Screen".
+
+// Three main apps -
+// The server App ("Server") (this file)
+// The User App ("Controller")
+// The Client App ("Screen")
+
+// This is a Node Js Program that uses "Express", "Socket io"
+// https://expressjs.com/
+// "Express is a minimal and flexible
+// Node.js web application framework
+// that provides a robust set of features
+// for web and mobile applications."
+// (Like Making an API Server.)
+
+// socket.io/
+// Socket.IO enables real-time,
+// bidirectional and event-based communication.
+// It works on every platform, browser or device,
+// focusing equally on reliability and speed.
+// (We use this to "Emit" a signal everytime
+// an API call is made to this server)
+
+// Intitialize "Express"
 const express = require("express");
 const app = express();
-// app.listen(3410, () => console.log("Listening on 3410"));
 
-// app.use(cors({ origin: true, credentials: true }));
+// process.env.PORT is when it's hosted online.
+// 9000 is when it's in developement/ localmachine:9000
 var server = app.listen(process.env.PORT || 9000, listen);
+
 // This call back just tells us that the server has started
 function listen() {
   var host = server.address().address;
@@ -12,15 +43,19 @@ function listen() {
   console.log("Example app listening at http://" + host + ":" + port);
 }
 
+// Host a Folder Called "public" and serve all the files within.
 app.use(express.static("public"));
+
+// Add a json parser
 app.use(express.json());
-// app.use(cors());
-// app.options("*", cors());
 
 // WebSocket Portion
 // WebSockets work with the HTTP server
-// var io = require("socket.io")(server);
 // server-side
+// Since the "User" app is also on the same server,
+// the CORS stuff below should be necessary,
+// but if the user app is on a different server,
+// you'll need the stuff below. (I think.)
 const io = require("socket.io")(server, {
   cors: {
     origin: "localhost:9000",
@@ -31,11 +66,27 @@ const io = require("socket.io")(server, {
   //   {transports: ['websocket']}
 });
 
+// This is where we combine the API (express) and Websockets (Socket io)
+// We create an API, at "/api",
+// with a call back function that receives the request
 app.post("/api", (request, res) => {
+  // For every API call we recieve, we "emit" a signal
+  // called "mouse" to the "Client" app, which in our
+  // case is the  "Screen". It also sends the data sent from the
+  // "user" app (Hue & Saturation) to the "client" app ("screen").
   io.emit("mouse", request.body);
+  // Console log the colours chosen by the user.
   console.log(request.body);
+  // If there is no response sent, the browser will stop
+  // sending anymore requests are 6 attempts or so.
+  // this line below sends out a response of "done". Will
+  // work with "" as well.
   res.end("done");
 });
+
+// This is how you may create a "GET" request, where the user
+// asks for some data and you can send it back. (I THINK)
+// At present it is not in use.
 app.get("/api", (request, response) => {
   console.log(request.body);
 });
--- public/public.js
+++ public/public.js
@@ -1,97 +1,146 @@
-let sendButton;
-let colour, h, s;
-let scene = 0;
+// Made by Raunak Sudhakar, as ViZrdLabs.
 
-function sendData() {
-  let data = {
-    hue: h,
-    saturation: s,
-  };
-  console.log(data);
-  let options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
-  fetch("/api", options, (response) => {
-    console.log(response);
-  });
-}
+// You need two devices to run it.
+// Demo
+// "Controller" on: https://fireburst.herokuapp.com/
+// "Screen" on: https://fireburst.herokuapp.com/screen/
+// Allows multiple devices as "controllers"
+// to send "Fireworks" to the "Screen".
 
+// Three main apps -
+// The server App ("Server")
+// The User App ("Controller") (this file)
+// The Client App ("Screen")
+
+// This Runs on "p5.js"
+// http://p5js.org/
+// p5.js is a JavaScript library for creative coding,
+// with a focus on making coding accessible and inclusive.
+
+// Declare the Variables we use.
+let sendButton; // Button
+let colour, h, s; // A colour variable, and h and s for hue and saturation.
+let scene = 0; // Scenes for splash screen or instructions.
+let sentTime = 0;
+
+// This is a p5 function
+// The setup() function is called once when the program starts.
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB);
+  colorMode(HSB); // colour mode as HUE SATURATION BRIGHTNESS mode.
   textAlign(CENTER);
+  ellipseMode(CENTER);
   background(200);
+
+  // Create a HTML button element and set parameters.
   sendButton = createButton("SEND\nFIREWORKS!");
   sendButton.size(100, 100);
   sendButton.style("background : #fff");
   sendButton.style("border-radius: 50px");
-  sendButton.position(width / 2, height / 2);
+  sendButton.position(width / 2 - 50, height / 2 - 50);
+  // Add the function "sendData" to the button.
   sendButton.mousePressed(sendData);
-  m = createVector(random(width), random(height));
-  s = map(m.x, 0, width, 0, 255);
-  h = map(m.y, 0, height, 0, 360);
-  colour = color(h, s, 200);
-  noStroke();
+
+  // pos is a vector to store the position of the circle on screen
+  pos = createVector(random(width), random(height));
   textSize(20);
 }
 
+// Called directly after setup(), the draw() function
+// continuously executes the lines of code contained
+// inside its block until the program is stopped or noLoop() is called.
 function draw() {
   switch (scene) {
     case 0:
-      background(220);
+      if (pos.x > width) {
+        pos.x = 0;
+      }
+      if (pos.y > height) {
+        pos.y = 0;
+      }
+      s = map(pos.x, 0, width, 0, 255);
+      h = map(pos.y, 0, height, 0, 360);
+      colour = color(h, s, 200);
+      background(220, 0.02);
       stroke(h, s, 150);
       strokeWeight(5);
       fill(h, s, 200, 0.6);
-      // noFill();
-      ellipse(m.x, m.y, 100);
-      m.x++;
-      m.y++;
-      if (m.x > width) {
-        m.x = 0;
-      }
-      if (m.y > height) {
-        m.y = 0;
-      }
-      s = map(m.x, 0, width, 0, 255);
-      h = map(m.y, 0, height, 0, 360);
-      colour = color(h, s, 200);
+      ellipse(pos.x, pos.y, 100);
+      pos.x++;
+      pos.y++;
       fill(20);
       noStroke();
       text("Choose a colour by\ntouching the screen", width / 2, 20);
       break;
     case 1:
       if (!mouseIsPressed) {
-        background(220);
+        background(220, 0.1);
       }
       fill(colour);
-      ellipse(m.x, m.y, 100);
+      ellipse(pos.x, pos.y, 100);
       break;
   }
 }
 
 function mouseDragged() {
-  scene = 1;
-  m.x = mouseX;
-  m.y = mouseY;
-  s = map(m.x, 0, width, 0, 255);
-  h = map(m.y, 0, height, 0, 360);
-  colour = color(h, s, 200);
+  if (
+    mouseX > width / 2 - 50 &&
+    mouseX < width / 2 + 50 &&
+    mouseY > height / 2 - 50 &&
+    mouseY < height / 2 + 50
+  ) {
+    // console.log("bingo");
+  } else {
+    scene = 1;
+    pos.x = mouseX;
+    pos.y = mouseY;
+    s = map(pos.x, 0, width, 0, 255);
+    h = map(pos.y, 0, height, 0, 360);
+    colour = color(h, s, 200);
 
-  background(colour, 0.1);
+    background(colour, 0.1);
+  }
 }
 
 function mousePressed() {
-  background(colour, 0.1);
-  scene = 1;
-  m.x = mouseX;
-  m.y = mouseY;
-  s = map(m.x, 0, width, 0, 255);
-  h = map(m.y, 0, height, 0, 360);
-  colour = color(h, s, 200);
+  if (
+    mouseX > width / 2 - 50 &&
+    mouseX < width / 2 + 50 &&
+    mouseY > height / 2 - 50 &&
+    mouseY < height / 2 + 50
+  ) {
+    // console.log("bingo");
+  } else {
+    background(colour, 0.1);
+    scene = 1;
+    pos.x = mouseX;
+    pos.y = mouseY;
+    s = map(pos.x, 0, width, 0, 255);
+    h = map(pos.y, 0, height, 0, 360);
+    colour = color(h, s, 200);
+    background(colour, 0.1);
+  }
+}
 
-  background(colour, 0.1);
+function sendData() {
+  if (sentTime + 1000 < millis()) {
+    let data = {
+      hue: h,
+      saturation: s,
+    };
+    // console.log(data);
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("/api", options, (response) => {
+      console.log(response);
+    });
+    sentTime = millis();
+  } else {
+    // console.log("Dont Spam I say!");
+  }
 }
